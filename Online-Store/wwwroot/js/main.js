b7d9fb23ec07b08968d57f8089bed779befe035d
laeof <turututuuuuuuu@gmail.com>
2023-07-31 19:20:04 +0300

    front-end update
    all pages are rewrited

--- Online-Store/wwwroot/js/main.js
+++ Online-Store/wwwroot/js/main.js
@@ -1,29 +1,89 @@
-const handleDomContentLoaded = function () {
-    const body = document.body;
-    const nav = document.querySelector('.navigation');
-    const burgerBtn = document.querySelector('header .nav-btn');
-    const burgetBtn2 = document.querySelector('.nav .nav-btn');
-    const overlay = document.querySelector('.header .overlay');
+const sidebar = document.getElementById('sidebar');
+const overlay = document.getElementById('overlay');
+const body = document.body;
 
-    function handleBurgerClick() {
-        if (nav.classList.contains('is-active')) {
-            closeNavigation();
-        } else {
-            openNavigation();
-        }
-    }
-
-    function closeNavigation() {
-        body.classList.remove('disable-scroll');
-        nav.classList.remove('is-active');
-    }
-
-    function openNavigation() {
+function toggleSideBar(){
+    if(sidebar.classList.contains('notactive')){
+        sidebar.classList.remove('notactive');
+        overlay.classList.add('active')
         body.classList.add('disable-scroll');
-        nav.classList.add('is-active');
     }
-    burgerBtn.addEventListener('click', handleBurgerClick);
-    burgetBtn2.addEventListener('click', handleBurgerClick);
-    nav.addEventListener('click', closeNavigation);
+    else{
+        sidebar.classList.add('notactive');
+        overlay.classList.remove('active');
+        body.classList.remove('disable-scroll');
+    }
 }
-document.addEventListener('DOMContentLoaded', handleDomContentLoaded);
+
+$(document).ready(function () {
+    let selectedStar = 0;
+
+    $(".star").hover(function () {
+        let starNumber = parseInt($(this).data("star"));
+
+        $(".star").removeClass("selected");
+
+        for (let i = 1; i <= starNumber; i++) {
+            $(`#star${i}`).addClass("selected");
+        }
+    }, function () {
+        $(".star").removeClass("selected");
+    });
+});
+
+overlay.addEventListener('click', toggleSideBar);
+
+const slidesContainer = document.querySelector('.slides');
+const slides = document.querySelectorAll('.slides img');
+const prevButton = document.querySelector('.prev-btn');
+const nextButton = document.querySelector('.next-btn');
+const dots = document.querySelectorAll('.dot');
+
+function updateActiveDot() {
+    dots.forEach((dot, index) => {
+        if (index === currentSlide) {
+            dot.classList.add('active');
+        } else {
+            dot.classList.remove('active');
+        }
+    });
+}
+
+dots.forEach((dot, index) => {
+    dot.addEventListener('click', () => {
+        currentSlide = index;
+        showSlide(currentSlide);
+        updateActiveDot();
+    });
+});
+
+prevButton.addEventListener('click', () => {
+    prevSlide();
+    updateActiveDot();
+});
+
+nextButton.addEventListener('click', () => {
+    nextSlide();
+    updateActiveDot();
+});
+
+let currentSlide = 0;
+
+function showSlide(slideIndex) {
+    slidesContainer.style.transform = `translateX(-${slideIndex * 100}%)`;
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+function prevSlide() {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide(currentSlide);
+}
+
+updateActiveDot();
+
+// Добавляем обработчики событий для кнопок "Next" и "Prev"
+document.addEventListener('DOMContentLoaded', showSlide(currentSlide));
